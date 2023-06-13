f8fe9ab1965d93b7518acba9252b5be300409262
Quertyy <qwertyense@protonmail.com>
2023-06-13 17:48:02 +0200

    add masterdog mock contract

---
+++ Swap/src/mock/MasterDog.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract MasterDog {
+    function viewAssetUserHolding(address user, address collection) external view returns (uint256[] memory) {
+        return new uint256[](1);
+    }
+}
